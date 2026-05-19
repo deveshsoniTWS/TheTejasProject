cbb1190e919213e1e1f9a2bee360c8b1f9e16573
deveshsoniTWS <devesh.soni@thoughtswinsystems.com>
2026-05-19 21:02:16 +0530

    added jwt time expiry variables in config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -8,4 +8,6 @@ export const config = {
   JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
   NODE_ENV: process.env.NODE_ENV || "development",
   SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || 10,
+  JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
+  JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
 };
