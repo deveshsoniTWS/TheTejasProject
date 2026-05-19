7442a241350c946b27bdf729a7cc598d15c8e8e6
deveshsoniTWS <devesh.soni@thoughtswinsystems.com>
2026-05-19 21:48:58 +0530

    routed the authRouter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { config } from "./config/config.js";
 import morgan from "morgan";
+import authRouter from "./modules/auth/auth.router.js";
 
 export const app = express();
 
@@ -10,6 +11,8 @@ app.set("port", config.PORT);
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("API running");
