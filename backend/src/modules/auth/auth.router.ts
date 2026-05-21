b40e7d467bfda985fcb9d1340ec133ec7a79c261
deveshsoniTWS <devesh.soni@thoughtswinsystems.com>
2026-05-21 16:18:30 +0530

    remove permission middle ware, input validation on login

--- backend/src/modules/auth/auth.router.ts
+++ backend/src/modules/auth/auth.router.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
 import { authenticateJWT } from "../../middleware/jwt.middleware";
-import { validateLoginBody } from "./middleware/login.validation";
 
 const authRouter = Router();
 const authController = new AuthController();
 
-authRouter.post("/login", validateLoginBody, authController.login);
+authRouter.post("/login", authController.login);
 authRouter.post("/refresh", authController.refresh);
 
 const protectedRouter = Router();
