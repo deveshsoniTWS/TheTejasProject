fd7f9298602c1048dd2edbaa0298c81a90db031f
deveshsoniTWS <devesh.soni@thoughtswinsystems.com>
2026-05-21 16:18:01 +0530

    salt round only in config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
   NODE_ENV: process.env.NODE_ENV || "development",
-  SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || 10,
+  SALT_ROUNDS: 10,
   JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
   JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
 };
