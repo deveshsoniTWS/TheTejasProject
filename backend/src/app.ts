4c4eeb201b240156f995c51ba45d36a3baf73e57
deveshsoniTWS <devesh.soni@thoughtswinsystems.com>
2026-05-25 12:55:59 +0530

    routed the role router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import authRouter from "./modules/auth/auth.router";
 import plantRouter from "./modules/plant/plant.router";
 import locationRouter from "./modules/location/location.router";
+import roleRouter from "./modules/role/role.router";
 
 export const app = express();
 
@@ -18,6 +19,7 @@ const apiRouter = express.Router();
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/plants", plantRouter);
 apiRouter.use("/locations", locationRouter);
+apiRouter.use("/roles", roleRouter)
 
 app.use("/api/v1", apiRouter);
 
